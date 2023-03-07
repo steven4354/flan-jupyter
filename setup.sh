67a634122f3be340112598166a87e35c83ad87a2
Steven Li <steven4354@gmail.com>
2023-03-07 13:52:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,28 @@
+# install git lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
+
+# clone down models
+cd
+
 git lfs
 git clone https://huggingface.co/google/flan-t5-xxl
+
+git lfs
+git clone https://huggingface.co/t5-large
+
+# install pip
 sudo apt update
 sudo apt install python3-pip
 pip3 --version
+sudo apt update 
+sudo apt install python2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+pip2 --version
+
+# prep jupyter notebook
+cd /flan-jupyter
+pip install -r requirements.txt
+
+
