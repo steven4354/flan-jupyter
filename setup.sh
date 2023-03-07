08b25960092fc79edf14fa8b0f8eee33a80c81ed
Steven Li <steven4354@gmail.com>
2023-03-07 13:46:33 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs
+git clone https://huggingface.co/google/flan-t5-xxl
+sudo apt update
+sudo apt install python3-pip
+pip3 --version
